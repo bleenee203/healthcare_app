e314bb0c72c045ada304a822d5d7d263b1df61ba
blee203 <vothibichly43@gmail.com>
2024-05-08 02:30:35 +0700

    add router for water log and watergoal

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:healthcare_app/app.dart';
 import 'package:healthcare_app/src/presentation/pages/cookie_manager.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   CookieManager.instance.initCookie();
   runApp(const MyApp());
 }
-
